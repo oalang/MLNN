8f188f4fd07e2a2823bdf0e08da618134e181d69
ol635o <olang1@wayfair.com>
2025-04-15 21:24:36 -0700

    Backup nonlinear exploration notebook

--- mlnn/engine.py
+++ mlnn/engine.py
@@ -830,6 +830,6 @@ class MLNNEngine:
         d = np.minimum(n_components, self.m)
         M = (np.clip(self.eigenvalues[self.m - d:], 0).reshape(d, 1) ** .5
              * self.eigenvectors[:, self.m - d:].T)
-        M[M[np.arange(d), np.argmax(M != 0, axis=1)] < 0] *= -1
+        # move to animation: M[M[np.arange(d), np.argmax(M != 0, axis=1)] < 0] *= -1
 
         return np.vstack((M, np.zeros((n_components - d, self.m))))
